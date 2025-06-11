0b3c64d9bb57f5c4c9de7bc08dbf6e1da6242b36
strongworldd <michotkevin271@gmail.com>
2025-06-11 11:50:15 +0200

    merge with testOSfiles

--- go.mod
+++ go.mod
@@ -1,14 +1,8 @@
 module forum
 
-<<<<<<< HEAD
 go 1.23.0
 
 require (
-	github.com/google/uuid v1.6.0 
-	github.com/mattn/go-sqlite3 v1.14.28 
+	github.com/google/uuid v1.6.0
+	github.com/mattn/go-sqlite3 v1.14.28
 )
-=======
-go 1.22.2
-
-require github.com/mattn/go-sqlite3 v1.14.28
->>>>>>> origin/testOSfiles
---
+++ tables/utilisateur.go
@@ -0,0 +1,99 @@
+package tables
+
+import (
+	"database/sql"
+	"fmt"
+
+	uuid "github.com/google/uuid"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func NewUser(Pseudo string, HashPass string, db *sql.DB) int {
+	statement, err := db.Prepare("INSERT INTO User (pseudo, password) VALUES(?,?)")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("error Prepare new user")
+		return (500)
+	}
+	statement.Exec(Pseudo, HashPass)
+	db.Close()
+	return (0)
+}
+
+func CheckPassword(username string, db *sql.DB) (int, string) {
+	var HashPass string
+	var password string
+
+	tsql, err := db.Query("SELECT password FROM User WHERE pseudo = (?)", username)
+	if err != nil {
+		fmt.Println(err)
+		return 500, HashPass
+	}
+
+	for tsql.Next() {
+		tsql.Scan(&password)
+	}
+	HashPass = password
+	return 0, HashPass
+}
+
+func GetId_User(username string, db *sql.DB) (int, int) {
+	var Id_user int
+
+	tsql, err := db.Query("SELECT Id_user FROM User WHERE pseudo = (?)", username)
+	if err != nil {
+		fmt.Println(err)
+		return 500, Id_user
+	}
+
+	for tsql.Next() {
+		tsql.Scan(&Id_user)
+	}
+	if Id_user > 0 {
+		return 0, Id_user
+	}
+	return 500, Id_user
+}
+
+func GetAllUsername(db *sql.DB) (int, []string) {
+	var allUsername []string
+	var username string
+
+	tsql, err := db.Query("SELECT pseudo FROM User")
+	if err != nil {
+		fmt.Println(err)
+		return 500, allUsername
+	}
+
+	for tsql.Next() {
+		tsql.Scan(&username)
+		allUsername = append(allUsername, username)
+	}
+	return 0, allUsername
+}
+
+func GetUserByUUID(uuid string, db *sql.DB) (int, string) {
+	var username string
+
+	tsql, err := db.Query("SELECT pseudo FROM User WHERE uuid = (?)", uuid) // check for UUID name in database
+	if err != nil {
+		fmt.Println(err)
+		return 400, username
+	}
+	for tsql.Next() {
+		tsql.Scan(&username)
+	}
+	return 0, username
+}
+
+func PutUUID(UUID uuid.UUID, pseudo string, db *sql.DB) int {
+	statement, err := db.Prepare("UPDATE User SET uuid = ? WHERE (pseudo =?)")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("error Prepare new user")
+		return (500)
+	}
+	statement.Exec(UUID, pseudo)
+	db.Close()
+	return (0)
+}
